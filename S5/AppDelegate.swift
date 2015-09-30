d2586c6dab446ff62542cd2f1d1e4314fc2adfab
Luo Sheng <luosheng1986@gmail.com>
2015-10-01 07:43:33 +0800

    Initial Commit

---
+++ S5/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  S5
+//
+//  Created by Luo Sheng on 15/10/1.
+//  Copyright © 2015年 Pop Tap. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
