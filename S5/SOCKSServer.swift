bb041ad101d26627d2ed744916e9642e1159ff0d
Luo Sheng <luosheng1986@gmail.com>
2015-10-01 11:40:07 +0800

    Add request phase enums.

--- S5/SOCKSServer.swift
+++ S5/SOCKSServer.swift
@@ -46,15 +46,43 @@ class SOCKSConnection: GCDAsyncSocketDelegate {
     enum SocketTag: UInt8 {
         case
 /* 
-+----+----------+----------+
-|VER | NMETHODS | METHODS  |
-+----+----------+----------+
-| 1  |    1     | 1 to 255 |
-+----+----------+----------+ 
+ +----+----------+----------+
+ |VER | NMETHODS | METHODS  |
+ +----+----------+----------+
+ | 1  |    1     | 1 to 255 |
+ +----+----------+----------+
 */
         HandshakeVersion = 5,
         HandshakeNumberOfAuthenticationMethods,
-        HandshakeAuthenticationMethod
+        HandshakeAuthenticationMethod,
+        
+/*
+ +----+-----+-------+------+----------+----------+
+ |VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
+ +----+-----+-------+------+----------+----------+
+ | 1  |  1  | X'00' |  1   | Variable |    2     |
+ +----+-----+-------+------+----------+----------+
+
+ o  VER    protocol version: X'05'
+ o  CMD
+ o  CONNECT X'01'
+ o  BIND X'02'
+ o  UDP ASSOCIATE X'03'
+ o  RSV    RESERVED
+ o  ATYP   address type of following address
+ o  IP V4 address: X'01'
+ o  DOMAINNAME: X'03'
+ o  IP V6 address: X'04'
+ o  DST.ADDR       desired destination address
+ o  DST.PORT desired destination port in network octet order
+*/
+        RequestHeaderFragment,
+        RequestAddressType,
+        RequestIPv4Address,
+        RequestIPv6Address,
+        RequestDomainNameLength,
+        RequestDomainName,
+        RequestPort
         
         func dataLength() -> Int {
             switch self {
