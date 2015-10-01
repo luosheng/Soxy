d61197e017bc6a7ae8473dd991065c5168c99aa8
Luo Sheng <luosheng1986@gmail.com>
2015-10-01 21:30:04 +0800

    Clean up unused code.

--- S5/SOCKSServer.swift
+++ S5/SOCKSServer.swift
@@ -62,74 +62,19 @@ class SOCKSConnection: GCDAsyncSocketDelegate, Equatable {
     static let version: UInt8 = 5
     static let replyTag = 100
     
-    enum SocketTag: UInt8 {
-        case
-/* 
- +----+----------+----------+
- |VER | NMETHODS | METHODS  |
- +----+----------+----------+
- | 1  |    1     | 1 to 255 |
- +----+----------+----------+
-*/
-        HandshakeVersion = 5,
-        HandshakeNumberOfAuthenticationMethods,
-        HandshakeAuthenticationMethod,
-        
-/*
- +----+-----+-------+------+----------+----------+
- |VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
- +----+-----+-------+------+----------+----------+
- | 1  |  1  | X'00' |  1   | Variable |    2     |
- +----+-----+-------+------+----------+----------+
-
- o  VER    protocol version: X'05'
- o  CMD
-    o  CONNECT X'01'
-    o  BIND X'02'
-    o  UDP ASSOCIATE X'03'
- o  RSV    RESERVED
- o  ATYP   address type of following address
-    o  IP V4 address: X'01'
-    o  DOMAINNAME: X'03'
-    o  IP V6 address: X'04'
- o  DST.ADDR       desired destination address
- o  DST.PORT desired destination port in network octet order
-*/
-        RequestHeaderFragment,
-        RequestAddressType,
-        RequestIPv4Address,
-        RequestIPv6Address,
-        RequestDomainNameLength,
-        RequestDomainName,
-        RequestPort
-        
-        func dataLength() -> Int {
-            switch self {
-                case .HandshakeVersion,
-                .HandshakeNumberOfAuthenticationMethods,
-                .RequestAddressType,
-                .RequestDomainNameLength:
-                return 1
-            case .RequestHeaderFragment:
-                return 3
-            case .RequestIPv4Address:
-                return 4
-            case .RequestIPv6Address:
-                return 16
-            case .RequestPort:
-                return 2
-            default:
-                return 0
-            }
-        }
-    }
-    
     enum Phase: Int {
         case
         MethodSelection = 10,
         Request
     }
     
+/*
+ +----+----------+----------+
+ |VER | NMETHODS | METHODS  |
+ +----+----------+----------+
+ | 1  |    1     | 1 to 255 |
+ +----+----------+----------+
+*/
     struct MethodSelection {
         let version: UInt8
         let numberOfAuthenticationMethods: UInt8
@@ -161,6 +106,13 @@ class SOCKSConnection: GCDAsyncSocketDelegate, Equatable {
         }
     }
     
+/*
+ +----+--------+
+ |VER | METHOD |
+ +----+--------+
+ | 1  |   1    |
+ +----+--------+
+*/
     struct MethodSelectionReply {
         let method: AuthenticationMethod
         
@@ -171,7 +123,27 @@ class SOCKSConnection: GCDAsyncSocketDelegate, Equatable {
             }
         }
     }
-    
+
+/*
+ +----+-----+-------+------+----------+----------+
+ |VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
+ +----+-----+-------+------+----------+----------+
+ | 1  |  1  | X'00' |  1   | Variable |    2     |
+ +----+-----+-------+------+----------+----------+
+
+ o  VER    protocol version: X'05'
+ o  CMD
+    o  CONNECT X'01'
+    o  BIND X'02'
+    o  UDP ASSOCIATE X'03'
+ o  RSV    RESERVED
+ o  ATYP   address type of following address
+    o  IP V4 address: X'01'
+    o  DOMAINNAME: X'03'
+    o  IP V6 address: X'04'
+ o  DST.ADDR       desired destination address
+ o  DST.PORT desired destination port in network octet order
+*/
     struct Request {
         enum Command: UInt8 {
             case
@@ -245,13 +217,6 @@ class SOCKSConnection: GCDAsyncSocketDelegate, Equatable {
         UsernamePassword
     }
     
-    enum RequestCommand: UInt8 {
-        case
-        Connect = 0x01,
-        Bind,
-        UDPAssociate
-    }
-    
     enum AddressType: UInt8 {
         case IPv4 = 0x01
         case IPv6 = 0x04
@@ -271,7 +236,6 @@ class SOCKSConnection: GCDAsyncSocketDelegate, Equatable {
         case InvalidDomainName
         case InvalidPort
     }
-    
     
 /*
  +----+-----+-------+------+----------+----------+
@@ -339,21 +303,16 @@ class SOCKSConnection: GCDAsyncSocketDelegate, Equatable {
     }
     
     var delgate: SOCKSConnectionDelegate?
-    private let clientSocket: GCDAsyncSocket
-    private var numberOfAuthenticationMethods = 0
-    private var requestCommand: RequestCommand = .Connect
-    private var domainLength = 0
-    private var targetHost: String?
-    private var targetPort: UInt16?
-    private var targetSocket: GCDAsyncSocket?
     private let delegateQueue: dispatch_queue_t
+    private let clientSocket: GCDAsyncSocket
+    private var targetSocket: GCDAsyncSocket?
     private var request: Request?
     
     init(socket: GCDAsyncSocket) {
         clientSocket = socket
         delegateQueue = dispatch_queue_create("net.luosheng.SOCKSConnection.DelegateQueue", DISPATCH_QUEUE_SERIAL)
         clientSocket.setDelegate(self, delegateQueue: delegateQueue)
-        self.beginHandshake()
+        clientSocket.readDataWithTimeout(-1, tag: Phase.MethodSelection.rawValue)
     }
     
     func disconnect() {
@@ -362,134 +321,6 @@ class SOCKSConnection: GCDAsyncSocketDelegate, Equatable {
     }
     
     // MARK: - Private methods
-    
-    private func beginHandshake() {
-        clientSocket.readDataWithTimeout(-1, tag: Phase.MethodSelection.rawValue)
-    }
-    
-    private func readSOCKSVersion(data: NSData) throws {
-        guard data.length == SocketTag.HandshakeVersion.dataLength() else {
-            throw SocketError.InvalidSOCKSVersion
-        }
-        var version: UInt8 = 0
-        data.getBytes(&version, length: data.length)
-        
-        guard version == SocketTag.HandshakeVersion.rawValue else {
-            throw SocketError.InvalidSOCKSVersion
-        }
-        clientSocket.readData(.HandshakeNumberOfAuthenticationMethods)
-    }
-    
-    private func readNumberOfAuthenticationMethods(data: NSData) throws {
-        guard (data.length == SocketTag.HandshakeNumberOfAuthenticationMethods.dataLength()) else {
-            throw SocketError.UnableToRetrieveNumberOfAuthenticationMethods
-        }
-        data.getBytes(&numberOfAuthenticationMethods, length: data.length)
-        clientSocket.readDataToLength(UInt(numberOfAuthenticationMethods), withTimeout: -1, tag: Int(SocketTag.HandshakeAuthenticationMethod.rawValue))
-    }
-    
-    private func readAuthenticationMethods(data: NSData) throws {
-        guard data.length == numberOfAuthenticationMethods else {
-            throw SocketError.WrongNumberOfAuthenticationMethods
-        }
-        var authMethods: [UInt8] = Array<UInt8>(count: numberOfAuthenticationMethods, repeatedValue: 0)
-        data.getBytes(&authMethods, length: numberOfAuthenticationMethods)
-        
-        guard authMethods.contains(AuthenticationMethod.None.rawValue) else {
-            throw SocketError.SupportedAuthenticationMethodNotFound
-        }
-        /*
-         +----+--------+
-         |VER | METHOD |
-         +----+--------+
-         | 1  |   1    |
-         +----+--------+
-        */
-        let methodSelectionBytes: [UInt8] = [SocketTag.HandshakeVersion.rawValue, AuthenticationMethod.None.rawValue];
-        let methodSelectionData = NSData(bytes: methodSelectionBytes, length: methodSelectionBytes.count)
-        clientSocket.writeData(methodSelectionData, withTimeout: -1, tag: 0)
-        clientSocket.readData(.RequestHeaderFragment)
-    }
-    
-    private func readHeaderFragment(data: NSData) throws {
-        guard data.length == SocketTag.RequestHeaderFragment.dataLength() else {
-            throw SocketError.InvalidHeaderFragment
-        }
-        
-        var header: [UInt8] = Array<UInt8>(count: data.length, repeatedValue: 0)
-        data.getBytes(&header, length: data.length)
-        
-        let version = header[0]
-        if (version != SocketTag.HandshakeVersion.rawValue) {
-            throw SocketError.InvalidSOCKSVersion
-        }
-        
-        guard let cmd = RequestCommand(rawValue: header[1]) else {
-            throw SocketError.InvalidRequestCommand
-        }
-        requestCommand = cmd
-        
-        // Reserved
-        _ = header[2]
-        
-        clientSocket.readData(.RequestAddressType)
-    }
-    
-    private func readAddressType(data: NSData) throws {
-        guard data.length == SocketTag.RequestAddressType.dataLength() else {
-            throw SocketError.InvalidAddressType
-        }
-        
-        var addressTypeByte: UInt8 = 0
-        data.getBytes(&addressTypeByte, length: data.length)
-        
-        guard let addressType = AddressType(rawValue: addressTypeByte) else {
-            throw SocketError.InvalidAddressType
-        }
-        
-        switch addressType {
-        case .IPv4:
-            clientSocket.readData(.RequestIPv4Address)
-            break
-        case .IPv6:
-            clientSocket.readData(.RequestIPv6Address)
-            break
-        case .DomainName:
-            clientSocket.readData(.RequestDomainNameLength)
-            break
-        }
-    }
-    
-    private func readDomainLength(data: NSData) throws {
-        guard data.length == SocketTag.RequestDomainNameLength.dataLength() else {
-            throw SocketError.InvalidDomainLength
-        }
-        
-        data.getBytes(&domainLength, length: data.length)
-        clientSocket.readDataToLength(UInt(domainLength), withTimeout: -1, tag: Int(SocketTag.RequestDomainName.rawValue))
-    }
-    
-    private func readDomainName(data: NSData) throws {
-        guard data.length == domainLength else {
-            throw SocketError.InvalidDomainName
-        }
-        
-        guard let domainName = String(data: data, encoding: NSASCIIStringEncoding) else {
-            throw SocketError.InvalidDomainName
-        }
-        targetHost = domainName
-        clientSocket.readData(.RequestPort)
-    }
-    
-    private func readPort(data: NSData) throws {
-        guard data.length == SocketTag.RequestPort.dataLength() else {
-            throw SocketError.InvalidPort
-        }
-        
-        var port: UInt16 = 0
-        data.getBytes(&port, length: data.length)
-        targetPort = port.bigEndian
-    }
     
     private func processMethodSelection(data: NSData) throws {
         var bytes: [UInt8] = [UInt8](count: data.length, repeatedValue: 0)
@@ -522,7 +353,6 @@ class SOCKSConnection: GCDAsyncSocketDelegate, Equatable {
     }
 
     @objc func socket(sock: GCDAsyncSocket!, didReadData data: NSData!, withTag tag: Int) {
-        print("data: \(data)")
         do {
             switch tag {
             case Phase.MethodSelection.rawValue:
@@ -566,12 +396,4 @@ class SOCKSConnection: GCDAsyncSocketDelegate, Equatable {
 
 func ==(lhs: SOCKSConnection, rhs: SOCKSConnection) -> Bool {
     return ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
-}
-
-// MARK: -
-
-extension GCDAsyncSocket {
-    func readData(tag: SOCKSConnection.SocketTag) {
-        return self.readDataToLength(UInt(tag.dataLength()), withTimeout: -1, tag: Int(tag.rawValue))
-    }
 }
