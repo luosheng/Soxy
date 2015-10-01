193a965539310a0c851c7d157e8df2fb5426ef3e
Luo Sheng <luosheng1986@gmail.com>
2015-10-01 08:41:13 +0800

    Implement GCDAsyncSocketDelegate.

--- S5/SOCKSServer.swift
+++ S5/SOCKSServer.swift
@@ -9,7 +9,7 @@
 import Foundation
 import CocoaAsyncSocket
 
-struct SOCKSServer {
+class SOCKSServer: GCDAsyncSocketDelegate {
     
     private let socket: GCDAsyncSocket
     private var connections: [SOCKSConnection]
@@ -17,12 +17,21 @@ struct SOCKSServer {
     init(port: UInt16) throws {
         connections = []
         socket = GCDAsyncSocket(delegate: nil, delegateQueue: dispatch_get_global_queue(0, 0))
-        socket.delegate = self as? AnyObject
+        socket.delegate = self
         try socket.acceptOnPort(port)
     }
     
+    // MARK: GCDAsyncSocketDelegate
+    
+    @objc func socket(sock: GCDAsyncSocket!, didAcceptNewSocket newSocket: GCDAsyncSocket!) {
+        let connection = SOCKSConnection(socket: newSocket)
+        connections.append(connection)
+    }
 }
 
 struct SOCKSConnection {
     
+    init(socket: GCDAsyncSocket) {
+        
+    }
 }
