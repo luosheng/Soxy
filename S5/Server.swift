b3e279347d0c701e4015d148f4fa25ca60b383a1
Luo Sheng <luosheng1986@gmail.com>
2015-10-03 22:07:03 +0800

    Add separator.

--- S5/Server.swift
+++ S5/Server.swift
@@ -50,7 +50,7 @@ public class Server: GCDAsyncSocketDelegate, ConnectionDelegate {
         connections.insert(connection)
     }
     
-    // MARK SOCKSConnectionDelegate
+    // MARK: - SOCKSConnectionDelegate
     
     func connectionDidClose(connection: Connection) {
         connections.remove(connection)
